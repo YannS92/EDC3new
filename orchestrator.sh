e2a8a63100a9ebb19b49f30fbd63c00c00c1fe41
Yann <73823501+YannS92@users.noreply.github.com>
2026-03-02 15:26:12 +0100

    local

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-browsers=("chrome" "firefox")
 viewports=("desktop" "mobile" "tablet")
 
 for viewport in "${viewports[@]}"; do
     echo "▶ Running $viewport"
-    VIEWPORT=$viewport docker compose run --rm tests
+    docker compose run --rm tests tests/ -v --headless -n auto --viewport=$viewport --reruns 1 --reruns-delay 2 --html=reports/report-$viewport.html --self-contained-html
 done
